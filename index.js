5a409c324a6ae0e1af2e31a795c9ff5442058793
seymurhesenzade <hasanzadeseymur21@gmail.com>
2024-04-24 00:28:23 +0400

    Calculator

--- index.js
+++ index.js
@@ -1,35 +1,29 @@
-const result = document.querySelector(".result");
-const netice = document.querySelector(".netice");
+const addition = document.querySelector(".addition")
+const substract = document.querySelector(".substract")
+const multiply = document.querySelector(".multiply")
+const division = document.querySelector(".division")
+const reset = document.querySelector(".reset")
+const sums = document.querySelector(".sums")
+const value1 = document.querySelector(".value1")
+const value2 = document.querySelector(".value2")
 
-const value1 = document.querySelector(".value1");
-const value2 = document.querySelector(".value2");
-const addBtn = document.querySelector(".addition");
-const subBtn = document.querySelector(".substract");
-const multBtn = document.querySelector(".multiply");
-const divBtn = document.querySelector(".division");
-const reset = document.querySelector(".reset");
-
-
-
-addBtn.addEventListener("click", (e) => {
-  let sum = +value1.value + +value2.value;
-  netice.textContent = sum;
-});
-subBtn.addEventListener("click", (e) => {
-  let sum = +value1.value - +value2.value;
-  netice.textContent = sum;
-});
-multBtn.addEventListener("click", (e) => {
-  let sum = +value1.value * +value.value;
-  netice.textContent = sum;
-});
-divBtn.addEventListener("click", (e) => {
-  let sum = +value1.value / +value2.value;
-  netice.textContent = sum;
-});
-
+addition.addEventListener("click", (e) => {
+    let sum = +value1.value + +value2.value;
+    sums.textContent = sum
+})
+substract.addEventListener("click", (e) => {
+    let sum = +value1.value - +value2.value;
+    sums.textContent = sum
+})
+multiply.addEventListener("click", (e) => {
+    let sum = +value1.value * +value2.value;
+    sums.textContent = sum
+})
+division.addEventListener("click", (e) => {
+    let sum = +value1.value / +value2.value;
+    sums.textContent = sum
+})
 reset.addEventListener("click", (e) => {
-  result.textContent = "0";
-  value1.value = 0;
-  value2.value = 0;
-});
+    value1.value = ""
+    value2.value = ""
+})
